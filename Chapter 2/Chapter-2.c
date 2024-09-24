4c3e86c2b03fcbe54b8081e37f0ebb289c42736c
Mario Fletcher <mariojfletcher@gmail.com>
2024-09-24 16:30:51 -0500

    Update Chapter-2.c

--- Chapter 2/Chapter-2.c
+++ Chapter 2/Chapter-2.c
@@ -1,1 +1,27 @@
+/* Prints a message on the screen */
 
+#include <stdio.h>
+
+main()
+
+{
+    printf("Just one small step for coders. One giant leap for");
+    printf(" programmers! \n");
+    return 0;
+}
+
+
+/* Second program, practice two working with the main, printf, C syntax */
+
+#include <stdio.h>
+
+main()
+
+{
+    printf("Hello world, this is my second attempt inputting C code.");
+    printf(" I have just completed Chapter %d\n", 2) ;
+    printf("I am % .1f percent ready to move on ", 99.9);
+    printf("to the next chapter!\n");
+
+    return 0;
+}
